9e6c1dcbda98ab70ae5ea69a6d18963068dbdca2
Johannes Ebner <Structed@users.noreply.github.com>
2023-10-07 19:58:59 +0200

    Initial commit

---
+++ Main.gd
@@ -0,0 +1,41 @@
+extends Control
+
+var cat_count = 0
+var dog_count = 0
+
+func _ready():
+	var _error = PlayFabManager.client.connect("api_error", _on_api_error)
+	PlayFabManager.client.connect("server_error", _on_api_error)
+	PlayFabManager.client.connect("logged_in", _on_login)
+	PlayFabManager.client.login_anonymous()
+
+
+func _on_api_error(api_error_wrapper: ApiErrorWrapper):
+	print_debug("API ERROR!" + api_error_wrapper.errorMessage)
+	
+	
+func _on_server_error(_api_error_wrapper):
+	print_debug("Server ERROR!")
+	
+
+func _on_login(_login_result: LoginResult):
+	%LoginStatusLabel.text = "Logged in"
+	%LoginStatusLabel.set("theme_override_colors/font_color", Color.WEB_GREEN)
+
+
+func _on_cats_button_pressed():
+	cat_count += 1
+	%CatCountLabel.text = str(cat_count)
+
+
+func _on_dogs_button_pressed():
+	dog_count += 1
+	%DogsCountLabel.text = str(dog_count)
+
+
+func _on_reset_button_pressed():
+	cat_count = 0
+	dog_count = 0
+	
+	%CatCountLabel.text = str(cat_count)
+	%DogsCountLabel.text = str(dog_count)
